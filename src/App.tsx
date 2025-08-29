8e90cee93f8e56b2313b8a16605f44659b999ec8
GATEWAYGROUP\Ziyauddin.Ansari <ziyauddin.ansari@thegatewaydigital.com>
2025-08-29 20:37:33 +0530

    fix: remove unnecessary Router component from App and wrap it in index with HashRouter

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 import { TodoProvider } from "./context/TodoContext";
 import Layout from "./components/layout/Layout";
 import Home from "./pages/Home";
@@ -11,17 +11,15 @@ import Contact from "./pages/Contact";
 function App() {
   return (
     <TodoProvider>
-      <Router>
-        <Layout>
-          <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/about" element={<About />} />
-            <Route path="/projects" element={<Projects />} />
-            <Route path="/todos" element={<Todos />} />
-            <Route path="/contact" element={<Contact />} />
-          </Routes>
-        </Layout>
-      </Router>
+      <Layout>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/projects" element={<Projects />} />
+          <Route path="/todos" element={<Todos />} />
+          <Route path="/contact" element={<Contact />} />
+        </Routes>
+      </Layout>
     </TodoProvider>
   );
 }
